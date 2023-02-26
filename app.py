24dd07ba13a4facdca022d6cf8d29e8266fc4dae
Muhammed Fais <fayismohd1123@gmail.com>
2023-02-26 13:17:33 +0400

    Flas App

--- app.py
+++ app.py
@@ -18,12 +18,21 @@ def predict_api():
     data=request.json['data']
     print(data)
     print(np.array(list(data.values())).reshape(1,-1))
-    new_data=scalar.transform(np.array(list(data.values())).reshape(1,-1))
+    new_data=scaler.transform(np.array(list(data.values())).reshape(1,-1))
     output=regmodel.predict(new_data)
     print(output[0])
     return jsonify(output[0])
 
-@app.route()
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_input=scaler.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output=regmodel.predict(final_input)[0]
+    return render_template('home.html',prediction_text='The house price predicted price is {}'.format(output))
+
+
+
 
 if __name__=='__name__':
     app.run(debug=True)
